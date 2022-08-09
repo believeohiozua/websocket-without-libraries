84d7a71903db9e9843348a13e087c2f12a394182
believeohiozua <believe.ohiozua@gmail.com>
2022-08-09 03:36:48 +0100

    sender optimizations

--- src/python/sender.py
+++ src/python/sender.py
@@ -4,8 +4,6 @@ from decouple import config
 from websocket import create_connection
 import time
 import json
-import re
-import ast
 
 
 import websockets
@@ -28,7 +26,7 @@ def fetch_batch(folder,batch_num):
         turn= file.read()
         return turn.split('//')
 
-
+'''
 #Start websocket transmission
 
 async def listen():
@@ -54,3 +52,52 @@ async def listen():
 
 asyncio.get_event_loop().run_until_complete(listen())
 # asyncio.get_event_loop().run_forever()
+
+'''
+
+import websocket
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import time
+
+def on_message(ws, message):
+    print("message: ", message)
+    
+
+def on_error(ws, error):
+    print("error: ", error)
+    
+
+def on_close(ws, message, error):
+    print(f"closed: {message} {error}")
+    
+
+def on_open(ws):
+    def run(*args):
+        count = 1
+        for msg in fetch_batch('hiroki1',1):
+            count+=1
+            data = {
+                "command": "new_frame", 
+                "sender": "py script", 
+                "frame": msg, 
+                "frameId": count, 
+                "keyFrameID": count, 
+            }
+            time.sleep(0.5)            
+            ws.send(json.dumps(data))
+            
+    thread.start_new_thread(run, ())
+
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://0.0.0.0:8000",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close
+                              )
+    ws.on_open = on_open
+    ws.run_forever()
