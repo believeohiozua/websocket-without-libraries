6d6c968633e38cdae4a65a86df48b65f838b1526
believeohiozua <believe.ohiozua@gmail.com>
2022-08-19 04:32:34 +0100

    stage 008

--- src/python/decoder.py
+++ src/python/decoder.py
@@ -1,9 +1,10 @@
 import boto3
 import base64
 import time
+from sender import send_message
 
-
-def fetch_batch(folder,batch_num):
+def fetch_batch(folder, batch_num):
+    print('\nbatch_num ', batch_num, '\n')
     s3 = boto3.client( "s3", region_name='eu-west-1' )
     with open('batch.bt', 'wb') as data:
         s3.download_fileobj("holotch-service-content", f'archive/{folder}/{batch_num}.bt', data) #hiroki1/1.bt
@@ -15,14 +16,9 @@ def fetch_batch(folder,batch_num):
         file.close()
         return to_binary
 
-      
-    
-            
-fetch_batch('hiroki1',2)
-
-
-
-def decoder(to_binary):    
+def decoder(to_binary, batch_no): 
+    # get total to_binary size 
+    total_size = len(to_binary)       
     size = int.from_bytes(to_binary[:1], byteorder='big' , signed=True)
     size2 = int.from_bytes(to_binary[1:2], byteorder='big' , signed=True)
     byte3 = int.from_bytes(to_binary[2:3], byteorder='big' , signed=True)
@@ -32,39 +28,56 @@ def decoder(to_binary):
     num_meshes = int.from_bytes(to_binary[12:16], byteorder='little', signed=True)
     valid_frame=[size,size2, byte3, byte4]
     if  valid_frame == [3,20,1,0]:
-        ''' if false skip to the next batch'''
+        ''' if false skip to the next batch'''        
         frame_size=0
         frame_count = 0
-        while True:   
-            try:                 
-                slice_frame = int.from_bytes(to_binary[frame_size:mesh_frame_size+frame_count], byteorder='little', signed=True)
-                frame = to_binary[:slice_frame] 
-                ''' convert binary to base64 encoded string '''
-                frame_base64 = base64.b64encode(frame)
-                frame_base64 = frame_base64.decode('utf-8')
-                frame_size=mesh_frame_size
-                frame_count += 1
-                
-                context =  {
-                        "command": "new_frame", 
-                        "sender": "js script", 
-                        "frame": frame_base64,
-                        "frameId": frame_count, 
-                        "keyFrameID": frame_count, 
-                    }
+        initial_mesh_frame_size=mesh_frame_size
+        while total_size >= frame_size:   
+            # try:                 
+            slice_frame = int.from_bytes(to_binary[frame_size:mesh_frame_size], byteorder='little', signed=True)
+            frame = to_binary[:slice_frame] 
+            #convert binary to base64 encoded string
+            frame_base64 = base64.b64encode(frame)
+            frame_base64 = frame_base64.decode('utf-8')            
+            frame_size=mesh_frame_size           
+            frame_count += 1
+            mesh_frame_size=initial_mesh_frame_size+frame_size
+            
+            context =  {
+                    "command": "new_frame", 
+                    "sender": "js script", 
+                    "frame": frame_base64,
+                    "frameId": frame_count, 
+                    "keyFrameID": frame_count, 
+                }
 
-                meta_data = {
-                    "timestamp":timestamp,
-                    "num_meshes":num_meshes,
-                    "frame":frame_base64,
-                    "mesh_frame_size":mesh_frame_size
-                    }
+            meta_data = {
+                "timestamp":timestamp,
+                "num_meshes":num_meshes,
+                "frame":frame_base64,
+                "mesh_frame_size":mesh_frame_size
+                }
 
-                time.sleep(0.5) 
-                return context
-            except Exception as e:
-                print(e)
-                break
+            # time.sleep(0.5) 
+            print(
+                f'''
+                \nbatch_no: {batch_no}
+                \nmesh_frame_size{mesh_frame_size}
+                \ntotal_size {total_size}
+                \nframe_count {frame_count}
+                \nframe_size: {frame_size}
+                \n\n''')
+
+            time.sleep(0.5)
+            send_message(context)
+
+        '''Start recursion to next batch'''
+        batch_no += 1
+        decoder(fetch_batch('hiroki1', batch_no), batch_no)
+
     else:
         print('Header is incorrect')
         return None
+
+
+decoder(fetch_batch('hiroki1',1),1)
--- src/python/sender.py
+++ src/python/sender.py
@@ -1,4 +1,3 @@
-from decoder import decoder, fetch_batch
 import json
 import ssl
 import websocket
@@ -6,11 +5,9 @@ import rel
 
 ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
 ws.connect("ws://0.0.0.0:8000")
-to_binary = fetch_batch('hiroki1',1)
-data= decoder(to_binary)
-ws.send(json.dumps(data))
-print(f'data sent!')
-# result =  ws.recv()
-'''end streaming manually with CTRL+C'''
-rel.signal(2, rel.abort) 
-rel.dispatch()
+def send_message(message):   
+    ws.send(json.dumps(message))
+    print(f'data sent!')
+    '''end streaming manually with CTRL+C'''
+    # rel.signal(2, rel.abort) 
+    # rel.dispatch()
